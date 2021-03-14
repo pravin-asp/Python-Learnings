9963bb3c630d4148bff215e80f33b784ce7b954b
Pravin Saravanabhavan <pravin.cs17@bitsathy.ac.in>
2021-03-14 16:40:35 +0530

    About Modules

---
+++ Modules.py
@@ -0,0 +1,17 @@
+# Modules --> file / folder
+	#Put necessary files in a folder is called modules
+	
+# Code Reusability
+
+# Abstraction --> Showing only necessary data and hiding unwanted one.
+
+# Eg : Car --> Accelerator, Gear, Clutch, Brake
+				# Showing only the fuctionality not the code
+				
+# If we want to use modules we have to use import 
+
+import math
+print(math.factorial(5))
+print(math.floor(5.2))
+print(math.ceil(5.2))
+print(int(math.pow(5, 3)))
