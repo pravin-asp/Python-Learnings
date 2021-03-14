8a07e8876f7bb99a33dca3e87796a96a94c7bcec
Pravin Saravanabhavan <pravin.cs17@bitsathy.ac.in>
2021-03-14 11:56:24 +0530

    output funcions and format specifiers

---
+++ Output_learning.py
@@ -0,0 +1,33 @@
+a, b, c = 100, 200, 300
+print(a, b, c)
+print(a, b, c, sep = ' : ')#at each and every space of output it works 
+print(a, b, c, end = ' - ')#at the end only it works
+
+
+#Formatted String
+# %i --> int 
+# %d --> int 
+# %f --> float 
+# %s --> string 
+print()
+a, b, c = 100, 200.09, 'Pravin'
+print('a value %d'%a)
+print('b value is %f and c is %s'%(b, c))
+
+name = 'Pravin'
+print('Welcome %s to my home'%name)
+
+
+
+#Replacement Operator
+
+name = 'Pravin'
+city = 'Tiruchengode'
+print('Hi this is {}. I am from {}'.format(name, city))
+print('Hi this is {0}. I am from {1}'.format(name, city))
+print('Hi this is {1}. I am from {0}'.format(name, city))
+
+name = input('Enter your name : ')
+print('lenght of name is %d'%len(name))
+mid = len(name) // 2
+print(name[: mid] + name[mid].upper() + name[mid + 1 :])
