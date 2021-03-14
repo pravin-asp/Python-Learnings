38305eeec6cc4d13e22df02319b6420230c458b4
Pravin Saravanabhavan <pravin.cs17@bitsathy.ac.in>
2021-03-14 11:54:13 +0530

    input function

---
+++ input_learning.py
@@ -0,0 +1,39 @@
+name = input("what is you name? ")
+print(type(name))
+
+num1 = input('Tell me a number')
+num2 = input('Tell me another number')
+print("Total is ", num1 + num2) #input is a string we have to typecaste
+
+#type casting
+
+num1 = int(num1)
+num2 = int(num2)
+print('Total is ', num1 + num2)
+
+
+#eval function
+
+result = eval('10 + 20 + 30')
+print(result)
+
+exp = input("Enter any arithmatic expression")
+result = eval(exp)
+print(result)
+
+
+#Command Line Arguments
+
+# Module -- Package -- Folder -- Files, Functions, 
+
+
+#input()
+#print()
+#eval()
+#int() -- Named set of code
+
+
+#python file_name.py 10 20 30
+#		argv[0]		[1] [2] [3]
+
+
