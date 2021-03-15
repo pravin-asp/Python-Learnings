eba3dd70dc29816586329a36940b29c2c202803c
Pravin Saravanabhavan <pravin.cs17@bitsathy.ac.in>
2021-03-15 10:55:27 +0530

    Additon of n num using for loop

---
+++ Addition_of_n_numbers.py
@@ -0,0 +1,15 @@
+# Additon of n numbers
+
+total = 0
+value = int(input())
+for i in range(1, value + 1):
+	total += i
+print(total)
+
+# Factorial of n numbers
+
+total = 1
+value = int(input())
+for i in range(1, value + 1):
+	total *= i 
+print(total)
