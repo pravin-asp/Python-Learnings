48eb5dd287b401eac38d2a64c5e36a5efec8f4a5
Pravin Saravanabhavan <pravin.cs17@bitsathy.ac.in>
2021-03-15 12:28:39 +0530

    Program 8

---
+++ Pattern08.py
@@ -0,0 +1,32 @@
+'''
+- - - - -
+- - - -
+- - -
+- -
+-
+'''
+row = 5
+while row >= 1:
+	col = 1
+	while col <= row:
+		print('-', end = ' ')
+		col += 1
+	print()
+	row -= 1
+	
+'''
+- - - - - 1
+- - - - 1
+- - - 1
+- - 1
+- 1
+'''
+row = 5
+while row >= 1:
+	col = 1
+	while col <= row:
+		print('-', end = ' ')
+		col += 1
+	print(1)
+	row -= 1
+
