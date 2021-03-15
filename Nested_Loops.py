f593c013f8016fdb4739406d5d86680fe7ecd900
Pravin Saravanabhavan <pravin.cs17@bitsathy.ac.in>
2021-03-15 11:55:30 +0530

    nested while loop

---
+++ Nested_Loops.py
@@ -0,0 +1,76 @@
+# 1 2 3 4 5 1 2 3 4 5 1 2 3 4 5
+
+col = 1
+while col <= 5:
+	print(col, end = ' ')
+	col += 1
+col = 1
+while col <= 5:
+	print(col, end = ' ')
+	col += 1
+col = 1
+while col <= 5:
+	print(col, end = ' ')
+	col += 1
+print()	
+print()
+print('---------------------------')
+print()
+
+row = 1
+while row <= 5:
+	col = 1
+	while col <= 5:
+		print(col, end = ' ')
+		col += 1
+	row += 1
+
+print()	
+print()
+print('---------------------------')
+print()
+
+# 12345
+# 12345
+# 12345
+
+row = 1
+while row <= 5:
+	col = 1
+	while col <= 5:
+		print(col, end = ' ')
+		col += 1
+	row += 1
+	print()
+	
+print()
+row = 1
+while row <= 5:
+	col = 1
+	while col <= 5:
+		print(row + col, end = ' ')
+		col += 1
+	row += 1
+	print()
+	
+	
+print()
+row = 1
+while row <= 5:
+	col = 1
+	while col <= 5:
+		print('*', end = ' ')
+		col += 1
+	row += 1
+	print()
+	
+	
+print()
+row = 1
+while row <= 5:
+	col = 1
+	while col <= row:
+		print(col, end = ' ')
+		col += 1
+	row += 1
+	print()
