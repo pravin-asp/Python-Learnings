25a0ff2db07c30d9ceac8dafc8a23cbc7d1f7b78
Pravin Saravanabhavan <pravin.cs17@bitsathy.ac.in>
2021-03-15 20:14:05 +0530

    String fuctions

---
+++ StringFun.py
@@ -0,0 +1,15 @@
+# startswith
+# endswith
+
+s = 'python is easy'
+print(s.startswith('python')) # sentence starts with the word
+print(s.endswith('python')) ## sentence ends with the word
+
+print(s.isalpha()) # checks it is only alphabets
+print(s.isdigit()) # checks it is only digits
+
+print(s.islower()) # checks it is lower
+print(s.isupper()) # checks it is upper
+
+print('Python Is Easy'.istitle())
+print(' '.isspace())
