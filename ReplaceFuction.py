ae25d381572dd5e0aaac6be9a9b8624abd463e99
Pravin Saravanabhavan <pravin.cs17@bitsathy.ac.in>
2021-03-15 19:40:52 +0530

    Replace function

---
+++ ReplaceFuction.py
@@ -0,0 +1,14 @@
+# Replace fuction
+
+# input --> raja
+# output --> Kaja
+
+# String is immutable
+
+s = 'raja'
+s2 = s.replace('r', 'k')
+print(s2)
+
+s = 'You cannot do'
+s2 = s.replace('not', '    ')
+print(s2)
