3dcbfc4c5fe6414e8d17099aaf62bf89d3594bed
Pravin Saravanabhavan <pravin.cs17@bitsathy.ac.in>
2021-03-15 11:28:11 +0530

    Updated for in string

--- Reverse_String_For.py
+++ Reverse_String_For.py
@@ -3,4 +3,16 @@ print(word)
 count = len(word)
 for i in range(count - 1, -1, -1):
 	print(word[i], end = '')
-print
+print()
+
+
+# Slicing
+
+# Step Operator
+print()
+word = 'amma'
+word1 = word[::-1]
+if word == word1:
+	print('Palindrome')
+else:
+	print('Not Palindrome')
