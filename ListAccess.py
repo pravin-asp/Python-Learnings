4cc620d10d03d6011a0d095f5c6eb32d3922facd
Pravin Saravanabhavan <pravin.cs17@bitsathy.ac.in>
2021-03-16 15:27:54 +0530

    List accessing

---
+++ ListAccess.py
@@ -0,0 +1,19 @@
+# List Accessing
+
+l = [10, 20, 'a', True, False]
+
+# Accessed using Index values
+print(l[0])
+print(l[2])
+
+# Negative indexing is also available
+print(l[-1])
+print(l[-3])
+
+# Slicing also done in list
+print(l[:])
+print(l[3:])
+
+# Accessing one by one
+for i in l:
+	print(i)
