a376675b6738a2d34bd009d43f86c0a20481b57a
Pravin Saravanabhavan <pravin.cs17@bitsathy.ac.in>
2021-03-19 14:54:30 +0530

    insert function

---
+++ insertFun.py
@@ -0,0 +1,14 @@
+# insert(a, b) --> used to add values at the given index
+# 		a --> index value, b --> value to be inserted
+
+
+l = [10,20,30,40,50]
+print(l)
+l.insert(3, 100)
+print(l)
+l.insert(100, 1000)
+print(l)
+l.insert(-100, 3)
+print(l)
+l.insert(-90, 4)
+print(l)
