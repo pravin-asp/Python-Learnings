f4f9bffa3a6ebd58a4441c115512b7a9b3b21c17
Pravin Saravanabhavan <pravin.cs17@bitsathy.ac.in>
2021-03-25 13:59:07 +0530

    Our own modules creation and using it

--- Modules.py
+++ Modules.py
@@ -9,9 +9,46 @@
 				# Showing only the fuctionality not the code
 				
 # If we want to use modules we have to use import 
-
+'''
 import math
 print(math.factorial(5))
 print(math.floor(5.2))
 print(math.ceil(5.2))
-print(int(math.pow(5, 3)))
+print(int(math.pow(5, 3)))'''
+
+# Group of functions, variables and classes
+import add
+#print(add.num)
+print(add.add())
+print(add.sub())
+print(add.add() * add.sub())
+
+# Module Aliasing
+
+import add as a 
+print(a.add())
+
+print()
+# only specific function from the modules
+from add import sub 
+print(sub())
+
+from add import add 
+print(add())
+
+print()
+import add as a
+print(a.add())
+print(a.sub())
+
+from add import * # * --> all members present in add modules
+print(add())
+print(sub())# dont want to give a.add() or add.add() just add() alone is enough
+
+import add
+#print(dir()) # dir() --> discreption about the modules and what are the functions are available
+print(dir(add))
+print('Program:',__name__)
+# __name__ :
+# modules.py --> python program --> __name__ ==> __main__
+# add.py --> module 
