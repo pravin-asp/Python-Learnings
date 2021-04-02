dddbe23266105c97f806fd0c7f87607869ed57a1
Pravin Saravanabhavan <pravin.cs17@bitsathy.ac.in>
2021-04-02 12:32:54 +0530

    OOPs Concepts

---
+++ OOPs.py
@@ -0,0 +1,59 @@
+# OOPs --> Object Oriented Programming Language
+
+# Procedure Oriented Programming Language
+# Functional Oriented Programming Langugae
+# Modular Programming
+
+# Paradigm - Karuthu
+
+# Object Oriented Programming Language is paradigm (idea)
+
+'''
+--> Object
+--> Class
+--> Inheritance
+--> Polymorphism
+--> Abstraction
+--> Encapsulation
+'''
+# In python everything is 
+# Python is a multi paradigm programming language
+
+# Object	- Real time entity
+#		 	- Combination of states and behaviour
+#			- Object can't be present without class
+#			- Object is memory reference of a class
+
+# Class - is a  template or is blueprint
+
+# Inheritance - generation qualities
+
+# HumanBeing - Template -> Class 
+
+# Object - states and behaviour
+
+# Code Reusability -- again and again using the same code
+# which means not using the code done by the some persong
+
+# Encapsulation Polymorphism Inheritance supports code reusability
+
+# Encapsulation  -- like giving protection to some things and giving permission like whatsapp status(who can see the status --> one type of Encapsulation)
+# - human being - personal - atm, lock, diary - personal
+# - personal - saying friends --> like module
+# - relatives --> marks
+# - public --> going to school, college
+
+# Encapsulation -- simply data protection
+#				-- data binding
+
+# Inheritance - like generation to generation
+#	- Object of one class behaving as an object of another class
+
+# Abstraction --> data hiding
+# Car : class --> steering, gear, accelertor, clutch, break --> how it works is hided
+#				  use is not hidden
+#				  Showing only the necessary data and hiding the unwanted one.
+
+# Polymorphism
+# Poly - many - பல
+# Showing different faces at different places
